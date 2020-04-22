ffa711e3d2cce2e10f97088ab44da13e4ece9b65
Boaz Ziniman <ziniman@gmail.com>
2020-04-22 14:00:29 +0300

    Moving to Paris

--- demo/parallel.js
+++ demo/parallel.js
@@ -1,17 +1,22 @@
 // Example POST method implementation:
 const url = 'https://232ojnqspg.execute-api.eu-west-1.amazonaws.com/send?user=boaz&message=Hello';
+var content = document.getElementById('content');
+var errorContent = document.getElementById('error');
 
-(async function() {
+async function callLambda() {
     try {
         var start = performance.now();
         const data = await postData(url);
         console.log(JSON.stringify(data));
         var end = performance.now();
         console.log("Lambda Call took " + (end - start) + " milliseconds.");
+        content.innerHTML += "<p>Lambda Call took " + (end - start) + " milliseconds.</p>";
     }catch (error) {
+      errorContent.style.display = "block";
+      errorContent.innerHTML += "<p>" + error + "</p>";
       console.error(error);
     }
-  })();
+  };
 
 async function postData(url) {
   const response = await fetch(url, {
@@ -27,3 +32,7 @@ async function postData(url) {
   await console.log(response);
   return await response.json(); // parses JSON response into native JavaScript objects
 }
+
+const interval = setInterval(function() {
+   callLambda()
+ }, 1000);
